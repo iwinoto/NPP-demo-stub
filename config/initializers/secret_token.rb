410cfdc02210f97802b43fddeec01dca3e9af5e4
Iwan Winoto <iwinoto@iwinoto-W530>
2015-07-12 00:46:19 +1000

    Initial checkin with specs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NPPMobilePaymentsApis::Application.config.secret_key_base = '218226dfadc3ee94a0517690517ec97a3b9af416a39cc99b71b11b42a03582b83bb4214c830ac1e57b1a8f8b0f2f542e20b75b6787caaa5dc3e00c96c6a79636'
